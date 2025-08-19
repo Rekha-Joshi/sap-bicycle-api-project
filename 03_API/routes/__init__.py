0e4d95ff834d80e02ea9ddbb9609ba8a13ccadb3
Rekha Joshi <rekhajoshi@Rekha-MacBook-Air.local>
2025-08-18 22:56:55 -0700

    Added new endpoint to create production order.

--- 03_API/routes/__init__.py
+++ 03_API/routes/__init__.py
@@ -3,4 +3,4 @@ from .vendors import vendors_bp
 from .materials import materials_bp
 from .employees import employees_bp
 from .sales_orders import sales_orders_bp
-from production_orders import production_orders_bp
+from .production_orders import production_orders_bp
--- 03_API/routes/production_orders.py
+++ 03_API/routes/production_orders.py
@@ -8,3 +8,42 @@ production_orders_bp = Blueprint("production_orders", __name__)
 #gloabal variable
 DB_PATH = "02_Database/bike_project.db"
 
+@production_orders_bp.route("/production_orders", methods = ["POST"])
+def create_prod_order():
+    data = request.get_json() or {}
+    if not data or "sales_order_id" not in data:
+        return jsonify({"Error":"Sales order id missing."}), 400
+    if not isinstance(data["sales_order_id"], int):
+        return jsonify({"error": "'Sales order id' must be a number"}), 400
+    if "planned_quantity" not in data:
+        return jsonify({"error": "planned_quantity is required"}), 400
+    try:
+        so_id = int(data["sales_order_id"])
+        planned_qty = int(data["planned_quantity"])
+        if planned_qty <= 0:
+            return jsonify({"error": "planned_quantity must be > 0"}), 400
+    except (TypeError, ValueError):
+        return jsonify({"error": "sales_order_id and planned_quantity must be numbers"}), 400
+    with sqlite3.connect(DB_PATH) as conn:
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM sales_orders where id = ? AND status = 'Pending'", (so_id,))
+        sales_order = cursor.fetchone()
+        if not sales_order: #if sales order not found, exit now
+            return jsonify({"Error":"Pending sales order not found."}), 404
+        cursor.execute("SELECT * FROM materials WHERE id = ? AND type = 'finished'", (sales_order["material_id"],))
+        materials = cursor.fetchone()
+        if not materials: #if material not found or not a finished product, exit now
+            return jsonify({"error": "Only finished products can be ordered"}), 404
+        cursor.execute("""
+                    INSERT INTO production_orders(sales_order_id, material_id, planned_quantity, start_date, 
+                    end_date, status) Values(?,?,?,?,?,?)
+                    """, (sales_order["id"], materials["id"], planned_qty, data["start_date"],
+                            data["end_date"],"Planned")
+        )
+        conn.commit()
+        order_id = cursor.lastrowid
+    return jsonify({
+        "message": "Production order created successfully.",
+        "order_id": order_id,
+    }), 201
